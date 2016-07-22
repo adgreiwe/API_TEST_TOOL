8fb5ebcda525b15d3ddb4e711f37ce6bc6e83235
adgreiwe <adgreiwe@hotmail.com>
2016-07-22 15:29:51 -0700

    backend

---
+++ BohUtilsApplication.java
@@ -0,0 +1,11 @@
+package com.sbux.boh.util.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author puchil
+ *
+ */
+@ApplicationPath("/")
+public class BohUtilsApplication extends Application{}
